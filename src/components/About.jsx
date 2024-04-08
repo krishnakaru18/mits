bc3ee4c274183248d75ade7b17ad2dfc5d81fd8d
krishnakaru18 <150501282+krishnakaru18@users.noreply.github.com>
2024-04-09 01:02:59 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,18 @@
-import { about } from "../assets"
-
+import { about } from "../assets";
 
 const About = () => {
-  
-  
   return (
-
-    // AboutSection.jsx
-
-    <section id='about' className="p-9 m-2 bg-white shadow-xl rounded-2xl">
-      <div className="container mx-auto">
-        <div className="flex flex-col md:flex-row items-center">
-          <div className="hover:bg-white transition duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 md:w-10/12 mb-6 md:mb-0">
-            <img src={about} alt="About" className="rounded-2xl" />
-          </div>
-          <div className="md:w-1/2 md:pl-10">
-            <h2 className="text-3xl font-semibold mb-4">About Event</h2>
-            <p className="text-slate-950 mb-1">
-            We are from "Madanapalle Institute of Technology and Science", Angallu. This year, we are thrilled to offer an incredible opportunity for all to participate in a One-day National Level Technical Symposium named "ECLECTICA 2K24," hosted by the Department of ECE.
-            Annually, the Electronics and Communication Department orchestrates a national level open-source talent program aimed at fostering innovative ideas and advancing technology. This year, we are equally excited to conduct this program with renewed enthusiasm. In this festival of intellect, you are invited to showcase your ideas through various events .
-            </p>
-            
-            <button className="bg-red-500 text-white px-6 py-3 rounded-md hover:bg-red-600 transition duration-300 mt-6 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"> Get Started
-                <a href="https://mits.ac.in/department/12">Learn More</a>
-            </button>
-          </div>
+    <section id='about' className="p-4 md:p-9 bg-gray-100 shadow-xl">
+      <div className="container mx-auto flex flex-col items-center">
+        <img src={about} alt="About" className="w-full mb-4 px-6" /> {/* Replace 'about' with the path to the resized image */}
+        <div className="text-black text-center">
+          <h2 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold mb-2">About Event</h2>
+          <p className="text-sm sm:text-base md:text-mb lg:text-lg mb-4 px-10">
+            This year, we are thrilled to offer an incredible opportunity for all to participate in a One-day National Level Technical Symposium named "ECLECTICA 2K24," hosted by the Department of ECE. Annually, the Electronics and Communication Department orchestrates a national level open-source talent program aimed at fostering innovative ideas and advancing technology. This year, we are equally excited to conduct this program with renewed enthusiasm.
+          </p>
+          <a href="https://mits.ac.in/department/12" className="inline-block  text-white px-6 py-3 rounded-md hover:bg-red-500 transition duration-300 transform hover:scale-105 focus:outline-none  bg-red-400 shadow-lg shadow-red-400/50 hover:shadow-xl hover:shadow-red-500/50 ...">
+            Visit Website
+          </a>
         </div>
       </div>
     </section>
@@ -32,4 +20,3 @@ const About = () => {
 };
 
 export default About;
-
