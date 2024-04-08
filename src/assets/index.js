3d76ae1ff48e66c4dea5e84a2a6b3fc0bcde846a
krishnakaru18 <150501282+krishnakaru18@users.noreply.github.com>
2024-04-09 01:14:05 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,7 @@
 import about from './about.jpg'
 import img2 from './ecletica.png'
+import logo1 from './logo1.png'
+import vid from './vid.mp4'
 
 import head from './head.png'
 import Mpl from './Mpl.png'
@@ -9,10 +11,11 @@ import poster from './poster.png'
 import project from './pro-expo.jpg'
 import quiz from './quiz.jpg'
 import Think from './Think.jpg'
-import vid from './vid.mp4'
 
 
-
+import find from './find.png'
+import photo from './photo.png'
+import pose from './pose.png'
 
 export {
     about,
@@ -25,5 +28,9 @@ export {
     quiz,
     Think,
     vid,
+    logo1,
+    find,
+    photo,
+    pose,
 }
 
