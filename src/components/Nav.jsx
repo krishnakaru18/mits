e751ba0d8035ddfec5235cecfb1dbf3ee2b90dd2
krishnakaru18 <150501282+krishnakaru18@users.noreply.github.com>
2024-04-29 13:37:36 +0530

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -33,7 +33,7 @@ const Nav = () => {
           <div className="relative md:hidden">
 
             <button onClick={toggleDropdown} className="p-3 focus:outline-none ">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 m-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 {isOpen ? (
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                 ) : (
